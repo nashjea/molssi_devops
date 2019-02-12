8b10f3b46e2c5114877be2eb5a042cef514c6858
Jessica Nash <janash@vt.edu>
2019-02-12 11:05:55 -0500

    add mean function to molssi_math

--- molssi_devops_2019/molssi_math.py
+++ molssi_devops_2019/molssi_math.py
@@ -6,6 +6,33 @@ Handles the primary functions
 """
 
 
+def mean(num_list):
+    """
+    Computes the mean of a list of numbers.
+
+    Parameters
+    ----------
+    num_list: list
+        List to calculate mean of
+
+    Returns
+    -------
+    list_mean: float
+        Mean of list of numbers
+    """
+
+    # Check that input is type list
+    if not isinstance(num_list, list):
+        raise TypeError('Invalid input %s - must be type list' % (num_list))
+    # Check that list has length greater than 0
+    if len(num_list) == 0:
+        raise ZeroDivisionError('Cannot calculate mean of empty list.')
+
+    list_mean = sum(num_list) / len(num_list)
+
+    return list_mean
+
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
